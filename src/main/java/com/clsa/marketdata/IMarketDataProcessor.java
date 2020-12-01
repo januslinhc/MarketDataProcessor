c4a79b421d573bbd81913a4ff6e6ee6e101e4e25
janus.lin <janus.lin@eddidfintech.com>
2020-12-01 20:59:28 +0800

    Update MarketDataProcessor

--- src/main/java/com/clsa/marketdata/IMarketDataProcessor.java
+++ src/main/java/com/clsa/marketdata/IMarketDataProcessor.java
@@ -1,5 +1,10 @@
 package com.clsa.marketdata;
 
 public interface IMarketDataProcessor {
+    /**
+     * Receive incoming market data
+     *
+     * @param data
+     */
     void onMessage(MarketData data);
 }
--- src/main/java/com/clsa/marketdata/MarketDataProcessor.java
+++ src/main/java/com/clsa/marketdata/MarketDataProcessor.java
@@ -2,11 +2,17 @@ package com.clsa.marketdata;
 
 import com.google.inject.Inject;
 
+import java.time.LocalDateTime;
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 public class MarketDataProcessor implements IMarketDataProcessor {
     private final HashMap<String, MarketData> dataHeap = new HashMap<>();
     private IRateLimiter rateLimiter = new RateLimiter(100);
+    private Deque<String> updated = new LinkedList<>();
+    private com.google.common.util.concurrent.RateLimiter rateLimiter2 = com.google.common.util.concurrent.RateLimiter.create(100);
 
     /**
      * Receive incoming market data
@@ -15,10 +21,26 @@ public class MarketDataProcessor implements IMarketDataProcessor {
      */
     @Override
     public void onMessage(MarketData data) {
-        while (!rateLimiter.acquire());
-        System.out.println(data.getAsk());
+        //System.out.println(data.getAsk());
+        rateLimiter.acquire();
+        if (!updated.contains(data.getSymbol())) {
+            updated.add(data.getSymbol());
+            publishAggregatedMarketData(data);
+        }
         dataHeap.put(data.getSymbol(), data);
-        publishAggregatedMarketData(data);
+        if (!updated.contains(data.getSymbol())) {
+            updated.add(data.getSymbol());
+        }
+        if (!rateLimiter2.tryAcquire()) {
+            rateLimiter2.acquire();
+            //System.out.println(updated.size());
+            System.out.println(LocalDateTime.now());
+            String symbol;
+            while (!updated.isEmpty()) {
+                symbol = updated.poll();
+                publishAggregatedMarketData(dataHeap.get(symbol));
+            }
+        }
     }
 
     /**
@@ -27,6 +49,7 @@ public class MarketDataProcessor implements IMarketDataProcessor {
      * @param data
      */
     public void publishAggregatedMarketData(MarketData data) {
+        System.out.println(data);
         // Do Nothing, assume implemented.
     }
 }
