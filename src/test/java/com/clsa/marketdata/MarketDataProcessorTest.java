c6469da03419fa0783c544544826616a2e6a79b0
janus.lin <janus.lin@eddidfintech.com>
2020-12-04 11:37:42 +0800

    Update test case

--- src/test/java/com/clsa/marketdata/MarketDataProcessorTest.java
+++ src/test/java/com/clsa/marketdata/MarketDataProcessorTest.java
@@ -1,24 +1,23 @@
 package com.clsa.marketdata;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static org.awaitility.Awaitility.await;
+import java.util.stream.Collectors;
 
 class MarketDataProcessorTest {
-    @ParameterizedTest(name = "when 101 symbols with {0} update entries each and each symbol update received per 100MS, {1} publishing in total is expected")
-    @CsvSource({"10,100", "11,200"})
-    void publishAggregatedMarketDataMethodIsNotCalledAnyMoreThan100TimesPerSec(int noOfSymbolRecords, int expectedTotalPublish) throws InterruptedException {
+    final List<String> symbols = Arrays.asList("A", "AAL", "AAP", "AAPL", "ABBV", "ABC", "ABMD", "ABT", "ACN", "ADBE", "ADI", "ADM", "ADP", "ADSK", "AEE", "AEP", "AES", "AFL", "AIG", "AIV", "AIZ", "AJG", "AKAM", "ALB", "ALGN", "ALK", "ALL", "ALLE", "ALXN", "AMAT", "AMCR", "AMD", "AME", "AMGN", "AMP", "AMT", "AMZN", "ANET", "ANSS", "ANTM", "AON", "AOS", "APA", "APD", "APH", "APTV", "ARE", "ATO", "ATVI", "AVB", "AVGO", "AVY", "AWK", "AXP", "AZO", "BA", "BAC", "BAX", "BBY", "BDX", "BEN", "BF", "BIIB", "BIO", "BK", "BKNG", "BKR", "BLK", "BLL", "BMY", "BR", "BRK", "BSX", "BWA", "BXP", "C", "CAG", "CAH", "CARR", "CAT", "CB", "CBOE", "CBRE", "CCI", "CCL", "CDNS", "CDW", "CE", "CERN", "CF", "CFG", "CHD", "CHRW", "CHTR", "CI", "CINF", "CL", "CLX", "CMA", "CMCSA");
+
+    //  Ensure that the publishAggregatedMarketData method is not called any more than 100 times/sec where this period is a sliding window.
+    @ParameterizedTest(name = "when {0} symbols with {1} update entries each and each symbol update received per 100MS, {2} publishing in total is expected")
+    @CsvSource({"100,10,100", "101,10,100", "100,11,200", "101,11,200"})
+    void publishAggregatedMarketDataMethodIsNotCalledAnyMoreThan100TimesPerSec(int noOfSymbols, int noOfSymbolRecords, int expectedTotalPublish) throws InterruptedException {
         List<MarketData> publishedEntries = new ArrayList<>();
         IMessageListener marketDataProcessor = new MarketDataProcessor() {
             @Override
@@ -27,10 +26,29 @@ class MarketDataProcessorTest {
                 publishedEntries.add(data);
             }
         };
-        final List<String> symbols = Arrays.asList("A", "AAL", "AAP", "AAPL", "ABBV", "ABC", "ABMD", "ABT", "ACN", "ADBE", "ADI", "ADM", "ADP", "ADSK", "AEE", "AEP", "AES", "AFL", "AIG", "AIV", "AIZ", "AJG", "AKAM", "ALB", "ALGN", "ALK", "ALL", "ALLE", "ALXN", "AMAT", "AMCR", "AMD", "AME", "AMGN", "AMP", "AMT", "AMZN", "ANET", "ANSS", "ANTM", "AON", "AOS", "APA", "APD", "APH", "APTV", "ARE", "ATO", "ATVI", "AVB", "AVGO", "AVY", "AWK", "AXP", "AZO", "BA", "BAC", "BAX", "BBY", "BDX", "BEN", "BF", "BIIB", "BIO", "BK", "BKNG", "BKR", "BLK", "BLL", "BMY", "BR", "BRK", "BSX", "BWA", "BXP", "C", "CAG", "CAH", "CARR", "CAT", "CB", "CBOE", "CBRE", "CCI", "CCL", "CDNS", "CDW", "CE", "CERN", "CF", "CFG", "CHD", "CHRW", "CHTR", "CI", "CINF", "CL", "CLX", "CMA", "CMCSA");
         final int msSleepTimeForEachSymbolRecord = 100;
-        generateData(marketDataProcessor, symbols, msSleepTimeForEachSymbolRecord, noOfSymbolRecords);
+        generateData(marketDataProcessor, symbols.stream().limit(noOfSymbols).collect(Collectors.toList()), msSleepTimeForEachSymbolRecord, noOfSymbolRecords);
         Assert.assertEquals(expectedTotalPublish, publishedEntries.size());
+    }
+
+    // Ensure each symbol will not have more than one update per second
+    @ParameterizedTest(name = "when 3 symbols with {0} update entries each and each symbol update received per 100MS, {1} update entries publishing for each symbol is expected")
+    @CsvSource({"0,0", "5,1", "11,2"})
+    void eachSymbolWillNotHaveMoreThanOneUpdatePerSecond(int noOfUpdateEntriesForEachSymbol, int expectedNoOfPublishingForEachSymbol) throws InterruptedException {
+        HashMap<String, Integer> publishCount = new HashMap<>();
+        IMessageListener marketDataProcessor = new MarketDataProcessor() {
+            @Override
+            public void publishAggregatedMarketData(MarketData data) {
+                super.publishAggregatedMarketData(data);
+                publishCount.put(data.getSymbol(), publishCount.getOrDefault(data.getSymbol(), 0) + 1);
+            }
+        };
+        final int msSleepTimeForEachSymbolRecord = 100;
+        List<String> inputSymbols = symbols.stream().limit(3).collect(Collectors.toList());
+        generateData(marketDataProcessor, inputSymbols, msSleepTimeForEachSymbolRecord, noOfUpdateEntriesForEachSymbol);
+        inputSymbols.forEach(s -> {
+            Assert.assertEquals(expectedNoOfPublishingForEachSymbol, publishCount.getOrDefault(s, 0).intValue());
+        });
     }
 
     private void generateData(IMessageListener marketDataProcessor, List<String> symbols, int sleepTimeForEachSymbolRecord, int noOfSymbolRecords) throws InterruptedException {
